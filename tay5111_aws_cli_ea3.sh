a4a34e06786b46a8a63f53034aae92ce8c2fb22f
EC2 Default User <ec2-user@ip-172-31-28-143.ec2.internal>
2024-01-30 20:26:33 +0000

    Update resource

--- tay5111_aws_cli_ea3.sh
+++ tay5111_aws_cli_ea3.sh
@@ -5,10 +5,12 @@
 #export AWS_SECRET_ACCESS_KEY="TU_SECRET_ACCESS_KEY"
 #export AWS_DEFAULT_REGION="us-east1"
 
-AWS_REGION="us-east1"
+AWS_REGION="us-east-1"
 
 # Obtener el VPC ID de la VPC "default"
 VPC_ID=$(aws ec2 describe-vpcs --filters "Name=isDefault,Values=true" --query 'Vpcs[0].VpcId' --output text)
+SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPC_ID" --query "Subnets[*].SubnetId" --output text)
+
 
 #Crear Security Groups
 aws ec2 create-security-group --group-name SG-Linux --description "Security Group for Linux instances" --vpc-id $VPC_ID --tag-specifications 'ResourceType=security-group,Tags=[{Key=Name,Value=SG-Linux}]'
@@ -44,10 +46,93 @@ aws s3api put-public-access-block \
 --bucket  $BUCKET_NAME \
 --public-access-block-configuration "BlockPublicAcls=false,IgnorePublicAcls=false,BlockPublicPolicy=false,RestrictPublicBuckets=false"
 
-aws s3api put-bucket-policy --bucket $BUCKET_NAME --policy file://policy.json
+cat <<EOF > policy.json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::$(echo $BUCKET_NAME)/*"
+        }
+    ]
+}
+EOF
 
+aws s3api put-bucket-policy --bucket $BUCKET_NAME --policy file://policy.json
 aws s3 cp index.php s3://tay5111/
 
+# EFS
 
+aws efs create-file-system \
+    --creation-token token \
+    --tags Key=Name,Value=webserver_EFS \
+    --performance-mode generalPurpose \
+    --throughput-mode bursting \
+    --encrypted \
+    --region us-east-1 \
+
+FILE_SYSTEM_ID=$(aws efs describe-file-systems --query "FileSystems[?Name=='webserver_EFS'].FileSystemId" --output text)
+
+for SUBNET_ID in $SUBNET_IDS; do
+    aws efs create-mount-target \
+    --file-system-id $FILE_SYSTEM_ID \
+    --subnet-id $SUBNET_ID \
+    --security-groups $SG_EFS_ID
+done
+
+
+echo "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $(echo $FILE_SYSTEM_ID).efs.us-east-1.amazonaws.com:/ /var/www/html"
+
+# Creacion de archivo user_data
+
+cat <<EOF > user_data.sh
+#!/bin/bash
+
+yum install -y httpd php wget
+systemctl start httpd
+systemctl enable httpd
+
+
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $(echo $FILE_SYSTEM_ID).efs.us-east-1.amazonaws.com:/ /var/www/html
+
+sudo wget -nc https://tay5111.s3.amazonaws.com/index.php -P /var/www/html/
+EOF
+
+#AMI_ID=$(aws ec2 describe-images --owners amazon --filters "Name=imageid,Values=ami-0a3c3a20c09d6f377" --query "Images[0].ImageId" --region $AWS_REGION --output text)
+AMI_ID="ami-0a3c3a20c09d6f377"
+
+# Obtener el ID de la subred en la zona de disponibilidad "us-east-1a"
+SUBNET_ID_A=$(aws ec2 describe-subnets --filters "Name=availability-zone,Values=us-east-1a" --query "Subnets[0].SubnetId" --output text)
+
+# Obtener el ID de la subred en la zona de disponibilidad "us-east-1b"
+SUBNET_ID_B=$(aws ec2 describe-subnets --filters "Name=availability-zone,Values=us-east-1b" --query "Subnets[0].SubnetId" --output text)
+
+# Obtener el ID de la subred en la zona de disponibilidad "us-east-1c"
+SUBNET_ID_C=$(aws ec2 describe-subnets --filters "Name=availability-zone,Values=us-east-1c" --query "Subnets[0].SubnetId" --output text)
+
+# Crear un array con los IDs de las subredes en cada zona de disponibilidad
+SUBNET_IDS=("$SUBNET_ID_A" "$SUBNET_ID_B" "$SUBNET_ID_C")
+AZ=("us-east-1a" "us-east-1b" "us-east-1c")
+i=0
+
+for SUBNET_ID in "${SUBNET_IDS[@]}"; do
+    INSTANCE_ID=$(aws ec2 run-instances \
+        --image-id $AMI_ID \
+        --instance-type t2.micro \
+        --security-group-ids $SG_Linux_ID \
+        --subnet-id $SUBNET_ID \
+        --associate-public-ip-address \
+        --user-data file://user_data.sh \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=webserver-${AZ[$i]}}]" \
+        --placement AvailabilityZone=$(aws ec2 describe-subnets --subnet-ids $SUBNET_ID --query "Subnets[0].AvailabilityZone" --output text) \
+        --query "Instances[0].InstanceId" \
+        --output text \
+        --region $AWS_REGION)
+    ((i++))
+done
+
+# ALB
 
 
--- user_data.sh
+++ user_data.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 yum install -y httpd php wget
 systemctl start httpd
 systemctl enable httpd
 
 
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-00023830db5cb174f.efs.us-east-1.amazonaws.com:/ /var/www/html
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-062a9d54702f1cfd6.efs.us-east-1.amazonaws.com:/ /var/www/html
 
-wget https://tay5111.s3.amazonaws.com/index.php -O /var/www/html/
+sudo wget -nc https://tay5111.s3.amazonaws.com/index.php -P /var/www/html/
